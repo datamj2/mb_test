a58960ce9e5d1a08dd192c8e267dc26081c16f93
datamj2 <mjansen2@gmail.com>
2017-11-13 17:46:10 -0800

    Initial Commit

---
+++ mb_test/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  mb_test
+//
+//  Created by mj on 11/13/17.
+//  Copyright © 2017 mj. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
